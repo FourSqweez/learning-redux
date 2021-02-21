be6b45c5d13cf8e578adc710cb1d7b2e301de0bf
FourSqweez <Kjeerasak@kkumail.com>
2021-02-21 15:49:29 +0700

    Added : state to input tag

--- src/comps/AddPost.js
+++ src/comps/AddPost.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react'
 
 class AddPost extends Component {
+  state = {
+    post: {
+      id: 4,
+      title: '',
+      content: '',
+    },
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      post: {
+        ...this.state.post,
+        [e.target.name]: e.target.value,
+      },
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(this.state.post)
+    this.setState({
+      post: {
+        title: '',
+        content: ''
+      },
+    })
+  }
+
   render() {
     return (
       <div className="container" style={{ margin: '4rem auto' }}>
         <h5 className="blue-text center-align">FourBlog</h5>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="input-field">
             <label htmlFor="post_title">Title</label>
-            <input type="text" name="title" />
+            <input
+              value={this.state.post.title}
+              onChange={this.handleChange}
+              type="text"
+              name="title"
+            />
           </div>
           <div className="input-field">
             <label htmlFor="post_content">Content</label>
             <textarea
+              value={this.state.post.content}
+              onChange={this.handleChange}
               name="content"
               className="materialize-textarea"
             ></textarea>
