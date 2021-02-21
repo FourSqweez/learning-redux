2f99b1f9d8a94fe522526b124688b186df56b540
FourSqweez <Kjeerasak@kkumail.com>
2021-02-21 15:27:36 +0700

    Added : redux with classes

---
+++ src/comps/Homepage.js
@@ -0,0 +1,13 @@
+import AddPost from './AddPost'
+import PostList from './PostList'
+
+const Homepage = () => {
+  return (
+    <>
+      <AddPost />
+      <PostList />
+    </>
+  )
+}
+
+export default Homepage
---
+++ src/store/reducers/postReducer.js
@@ -0,0 +1,20 @@
+const initState = {
+  posts: [
+    { id: 1, title: 'post one', content: 'this is post one' },
+    { id: 2, title: 'post two', content: 'this is post two' },
+    { id: 3, title: 'post three', content: 'this is post three' },
+  ],
+}
+const postReducer = (state = initState, action) => {
+  switch (action.type) {
+    case 'DELETE_POST':
+      const newPost = state.posts.filter((post) => post.id !== action.id)
+      return {
+        posts: newPost,
+      }
+    default:
+      return state
+  }
+}
+
+export default postReducer
